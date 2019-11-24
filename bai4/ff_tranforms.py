87b9c00c4ac9a7b63eb8dc7cc441d3cf9c7ba4c4
khai9xht <khaihoang9x@gmail.com>
2019-11-24 22:37:40 +0700

    abcskaclac

--- bai4/ff_tranforms.py
+++ bai4/ff_tranforms.py
@@ -1,14 +1,25 @@
-import scipy
-import matplotlib.pyplot as plt
 import numpy as np
-
+import matplotlib.pyplot as plt
 from scipy import fftpack
-import cv2
+from matplotlib.colors import LogNorm
 
-path_image = 'test.jpg'
-image = cv2.imread(path_image)
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('test_gray.jpg', gray_image)
+im = plt.imread('test.jpg').astype(float)
 
-fft_image = fftpack.fft2(image)
-cv2.imwrite('test_fft.jpg', fft_image)
+plt.figure()
+plt.imshow(im, plt.cm.gray)
+plt.title('Original image')
+plt.savefig('gray_test.jpg')
+
+
+im_fft = fftpack.fft2(im)
+plt.figure()
+plt.imshow(np.abs(im_fft), norm=LogNorm(vmin=5))
+plt.colorbar()
+plt.title('Fourier transform')
+plt.savefig('fft_test.jpg')
+
+im_new = fftpack.ifft2(im_fft).real
+plt.figure()
+plt.imshow(im_new, plt.cm.gray)
+plt.title('Reconstructed Image')
+plt.savefig('reconstruct_image.jpg')
